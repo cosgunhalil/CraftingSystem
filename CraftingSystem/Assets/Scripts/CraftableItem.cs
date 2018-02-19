40c2a93491b6c464a76b4237984adfa688318aea
halil cosgun <cosgun.halil@gmail.com>
2018-02-20 00:16:16 +0300

    Base classes are created

---
+++ CraftingSystem/Assets/Scripts/CraftableItem.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CraftableItem : MonoBehaviour {
+
+    public CraftableItem[] _components;
+
+    public virtual void Init()
+    {
+
+    }
+
+    public virtual void Craft()
+    {
+
+    }
+
+    public virtual void DeCompose()
+    {
+
+    }
+
+}
